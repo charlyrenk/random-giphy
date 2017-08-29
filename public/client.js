e179b7feb1d6d47e96d7bb4e4205fd860afc11c8
Charles Renk <renkx008@umn.edu>
2017-08-29 12:01:08 -0500

    added gif search

--- public/client.js
+++ public/client.js
@@ -31,12 +31,26 @@ app.controller('SwapiController', ['$http', function($http) {
         console.log('baseUrl: ', baseUrl);
         
         $http.get(baseUrl).then(function(response) {
-            console.log('trending giphys ', response.data);
+            console.log('random giphys ', response.data);
             self.giphyResult = response.data;
         });
     };
 
     // self.getTrendingGiphy();
+    self.searchGiphy = function(search) {
+        // example request:
+        // http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
+        var baseUrl = 'http://api.giphy.com/v1/gifs/search?q=' + search;
+        baseUrl += '&api_key=' + giphyAPIKey;    // api key
+        baseUrl += '&limit=5';  // limit
+        baseUrl += '&rating=g'; // rating
 
+        console.log('baseUrl: ', baseUrl);
+        
+        $http.get(baseUrl).then(function(response) {
+            console.log('searched giphys ', response.data);
+            self.giphyResult = response.data;
+        });
+    };
     
 }]);
