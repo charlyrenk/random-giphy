78ed30dc396b4faf8cabf4a2e6c67c14d6c74fdf
Kris Szafranski <kdszafranski@gmail.com>
2017-08-29 10:23:54 -0500

    basic giphy trending api request. not on the DOM

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ app.controller('SwapiController', ['$http', function($http) {
     console.log('controller loaded');
 
     var self = this;
+    var giphyAPIKey = '';   // your API Key
 
     self.getSpecies = function(id) {
 
@@ -18,6 +19,24 @@ app.controller('SwapiController', ['$http', function($http) {
             });
         });
     }
+
+    self.getTrendingGiphy = function() {
+        // example request:
+        // http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
+        var baseUrl = 'http://api.giphy.com/v1/gifs/trending?';
+        baseUrl += 'api_key=' + giphyAPIKey;    // api key
+        baseUrl += '&limit=5';  // limit
+        baseUrl += '&rating=g'; // rating
+
+        console.log('baseUrl: ', baseUrl);
+        
+        $http.get(baseUrl).then(function(response) {
+            console.log('trendaing giphys ', response.data);
+            
+        });
+    };
+
+    // self.getTrendingGiphy();
 
     
 }]);
